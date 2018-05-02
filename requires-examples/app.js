65e9ffbee159ef45f4b19e4b5b1b6f2adddf8ea6
Arunachalam <arunsha0007@gmail.com>
2018-05-02 16:26:26 -0700

    Tested require  for externalFile

--- requires-examples/app.js
+++ requires-examples/app.js
@@ -1,7 +1,11 @@
 console.log("This is requires example program");
 
+// added build in files 
 const fs = require('fs');
 const os = require('os');
 
+// added external files
+const exFile=require('./externalFile1');
+
 var user = os.userInfo();
-fs.appendFile('TextFile.txt', `hello ${user.username}!`);
+fs.appendFile('TextFile.txt', `hello ${user.username}!`+" You're age is: " +exFile.age+" You're teamName is: "+exFile.teamName);
---
+++ requires-examples/externalFile1.js
@@ -0,0 +1,5 @@
+console.log("external file launched");
+
+module.exports.age = 20;
+module.exports.teamName = 'Documentum Team';
+// var teamName = 'Documentum Team'; // undefined
