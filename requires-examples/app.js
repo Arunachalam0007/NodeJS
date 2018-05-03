c31d7124a7fe501cf91b910dfc812ae959cc224d
Arunachalam <arunsha0007@gmail.com>
2018-05-02 19:06:29 -0700

    Done with 3rd party model of lodash without importing nodemodules

--- requires-examples/app.js
+++ requires-examples/app.js
@@ -7,5 +7,17 @@ const os = require('os');
 // added external files
 const exFile=require('./externalFile1');
 
+// added third party models 
+
+const _=require('lodash'); //npm install lodash --save
+
 var user = os.userInfo();
-fs.appendFile('TextFile.txt', `hello ${user.username}!`+" You're age is: " +exFile.age+" You're teamName is: "+exFile.teamName);
+fs.appendFile('TextFile.txt', `hello ${user.username}!`+" You're age is: " +exFile.age+" You're teamName is: "+exFile.teamName+"\n");
+fs.appendFile('TextFile.txt',exFile.details());
+
+//lodash methods 
+console.log(_.isString(true)); //false
+console.log(_.isString("Arunsha")); //true
+
+var filteredArray = _.uniq(['Arun',101,102,101,'sha','Arun']);
+console.log(filteredArray);  //[ 'Arun', 101, 102, 'sha' ]
--- requires-examples/externalFile1.js
+++ requires-examples/externalFile1.js
@@ -2,4 +2,7 @@ console.log("external file launched");
 
 module.exports.age = 20;
 module.exports.teamName = 'Documentum Team';
+module.exports.details=()=>{
+    return "You're age is: "+this.age + "You're teamName is:" +this.teamName;
+}
 // var teamName = 'Documentum Team'; // undefined
